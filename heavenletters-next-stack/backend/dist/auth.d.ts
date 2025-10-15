e1a3dc6faef7252962417b7c809bcc70298a755c
wunluv <santhan@mojahmedia.net>
2025-10-15 10:03:11 +0200

    Updated structure of repos

--- heavenletters-next-stack/backend/dist/auth.d.ts
+++
@@ -1,3 +0,0 @@
-declare const withAuth: <TypeInfo extends import("@keystone-6/core/types").BaseKeystoneTypeInfo>(config: import("@keystone-6/core/types").KeystoneConfig<TypeInfo>) => import("@keystone-6/core/types").KeystoneConfig<TypeInfo>;
-declare const session: import("@keystone-6/core/types").SessionStrategy<unknown, any>;
-export { withAuth, session };
--- heavenletters-next-stack/backend/dist/keystone.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const keystoneConfig: import("@keystone-6/core/types").KeystoneConfig<any>;
-declare const _default: import("@keystone-6/core/types").KeystoneConfig<any>;
-export default _default;
